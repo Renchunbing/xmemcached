59bcb84524fd07a17fd017e172a040848df083a7
killme2008 <killme2008@e2e35160-022d-11de-bc25-5f29055a2a2d>
2009-06-06 07:45:14 +0000

    Add/Remove servers UnitTest

--- src/net/rubyeye/xmemcached/test/PerformanceTest.java
+++ src/net/rubyeye/xmemcached/test/PerformanceTest.java
@@ -17,6 +17,10 @@ public class PerformanceTest {
 	 * @author dennis
 	 * 
 	 */
+	/**
+	 * @author dennis
+	 *
+	 */
 	static class TestWriteRunnable implements Runnable {
 
 		private MemcachedClient mc;
